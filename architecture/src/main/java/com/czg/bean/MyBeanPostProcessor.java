5badb70c935d6292cea98ce660114b2c243c84b9
zhigong.chen <781627757@qq.com>
2018-09-26 22:56:32 +0800

    append

---
+++ architecture/src/main/java/com/czg/bean/MyBeanPostProcessor.java
@@ -0,0 +1,34 @@
+package com.czg.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * @author chenzg
+ * @date 2018.09.03 11:36
+ * @description
+ **/
+public class MyBeanPostProcessor implements BeanPostProcessor {
+
+    public MyBeanPostProcessor() {
+        super();
+        System.out.println("这是BeanPostProcessor实现类构造器！！");
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object arg0, String arg1)
+            throws BeansException {
+        System.out
+                .println("BeanPostProcessor接口方法postProcessAfterInitialization对属性进行更改！");
+        return arg0;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object arg0, String arg1)
+            throws BeansException {
+        System.out
+                .println("BeanPostProcessor接口方法postProcessBeforeInitialization对属性进行更改！");
+        return arg0;
+    }
+}
